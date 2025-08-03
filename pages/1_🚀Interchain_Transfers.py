2ba597a2adeba5b841a9d2a53c56d94c658064bd
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-03 04:06:15 +0330

    Update 1_🚀Interchain_Transfers.py

--- pages/1_🚀Interchain_Transfers.py
+++ pages/1_🚀Interchain_Transfers.py
@@ -1,1 +1,51 @@
+import streamlit as st
+import pandas as pd
+import snowflake.connector
+import plotly.express as px
+import plotly.graph_objects as go
+
+# --- Page Config: Tab Title & Icon -------------------------------------------------------------------------------------
+st.set_page_config(
+    page_title="Axelar : Interchain Transactions Overview",
+    page_icon="https://img.cryptorank.io/coins/axelar1663924228506.png",
+    layout="wide"
+)
+
+# --- Title with Logo ---------------------------------------------------------------------------------------------------
+st.markdown(
+    """
+    🚀Interchain Transfers
+    """
+)
+
+st.info(
+    "📊Charts initially display data for a default time range. Select a custom range to view results for your desired period."
+
+)
+
+st.info(
+    "⏳On-chain data retrieval may take a few moments. Please wait while the results load."
+)
+
+# --- Snowflake Connection --------------------------------------------------------------------------------------------------
+conn = snowflake.connector.connect(
+    user=st.secrets["snowflake"]["user"],
+    password=st.secrets["snowflake"]["password"],
+    account=st.secrets["snowflake"]["account"],
+    warehouse="SNOWFLAKE_LEARNING_WH",
+    database="AXELAR",
+    schema="PUBLIC"
+)
+
+# --- Time Frame & Period Selection ---
+timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
+start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("2025-07-31"))
+
+# --- Query Functions ---------------------------------------------------------------------------------------
+
+
+# --- Load Data ----------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------------
 
