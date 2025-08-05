4184b95a4b5295d36b0bd1b98716649c120f9a2c
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-05 17:04:33 +0330

    Update 3_💎Routes_&_Tokens.py

--- pages/3_💎Routes_&_Tokens.py
+++ pages/3_💎Routes_&_Tokens.py
@@ -28,11 +28,6 @@ conn = snowflake.connector.connect(
     schema="PUBLIC"
 )
 
-# --- Time Frame & Period Selection --------------------------------------------------------------------------------------
-timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
-start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
-end_date = st.date_input("End Date", value=pd.to_datetime("2025-07-31"))
-
 # -------------------------------------------------------------------------------------------------------------------------
 # --- Platform & API Mapping --------------------------------------------------------------------------------------------
 platform_apis = {
