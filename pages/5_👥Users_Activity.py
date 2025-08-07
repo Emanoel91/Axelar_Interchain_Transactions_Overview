0785508ab0d949c57b62ba532459bf5c753c7581
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-07 14:40:55 +0330

    Update 5_👥Users_Activity.py

--- pages/5_👥Users_Activity.py
+++ pages/5_👥Users_Activity.py
@@ -1,1 +1,34 @@
+import streamlit as st
+import pandas as pd
+import requests
+import snowflake.connector
+import plotly.graph_objects as go
+import plotly.express as px
 
+# --- Page Config: Tab Title & Icon -------------------------------------------------------------------------------------
+st.set_page_config(
+    page_title="Axelar : Interchain Transactions Overview",
+    page_icon="https://img.cryptorank.io/coins/axelar1663924228506.png",
+    layout="wide"
+)
+
+# --- Title & Info Messages ---------------------------------------------------------------------------------------------
+st.title("👥Users Activity")
+
+st.info("📊 Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
+st.info("⏳ On-chain data retrieval may take a few moments. Please wait while the results load.")
+
+# --- Snowflake Connection ----------------------------------------------------------------------------------------------
+conn = snowflake.connector.connect(
+    user=st.secrets["snowflake"]["user"],
+    password=st.secrets["snowflake"]["password"],
+    account=st.secrets["snowflake"]["account"],
+    warehouse="SNOWFLAKE_LEARNING_WH",
+    database="AXELAR",
+    schema="PUBLIC"
+)
+
+# --- Time Frame & Period Selection --------------------------------------------------------------------------------------
+# -- timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
+start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("2025-07-31"))
